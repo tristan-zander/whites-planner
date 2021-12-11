0c6bd2d49f6540479d78124544a98ea756b29e33
Tristan Zander <knavetnz@gmail.com>
2021-12-10 20:40:14 -0500

    Update footer text

--- components/Footer.js
+++ components/Footer.js
@@ -5,11 +5,7 @@ import netliheart from "../public/netliheart.svg";
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with{" "}
-        <Image src={netliheart} alt="Netlify Logo" height={30} width={30} />
-        for you
-      </footer>
+      <footer className={styles.footer}>Currently under construction</footer>
     </>
   );
 }
