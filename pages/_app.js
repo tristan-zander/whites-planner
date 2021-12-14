ba7e99abbbd240ce6972b83205c5df53f3bd0684
Tristan Zander <knavetnz@gmail.com>
2021-12-13 22:19:18 -0500

    Added template assignment and validation

--- pages/_app.js
+++ pages/_app.js
@@ -3,40 +3,46 @@ import "@styles/globals.css";
 import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
-import { Box, ThemeProvider, createTheme } from "@mui/material";
-import { resetServerContext, DragDropContext } from "react-beautiful-dnd";
+import { Box, ThemeProvider, createTheme, CssBaseline } from "@mui/material";
+import { resetServerContext } from "react-beautiful-dnd";
 import { Provider } from "react-redux";
 import store from "../features/store";
+import { LocalizationProvider } from "@mui/lab";
+import DateAdapter from "@mui/lab/AdapterLuxon";
 
 function Application({ Component, pageProps }) {
   resetServerContext();
   return (
-    <ThemeProvider theme={createTheme()}>
-      <Provider store={store}>
-        <Box
-          sx={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Head>
-            <title>White&#39;s Planner</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
+    <CssBaseline>
+      <ThemeProvider theme={createTheme()}>
+        <LocalizationProvider dateAdapter={DateAdapter}>
+          <Provider store={store}>
+            <Box
+              sx={{
+                height: "100vh",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Head>
+                <title>White&#39;s Planner</title>
+                <link rel="icon" href="/favicon.ico" />
+              </Head>
 
-          <Header title="White's Planner" />
-          <Box
-            sx={{
-              height: "100%",
-            }}
-          >
-            <Component {...pageProps} />
-          </Box>
-          <Footer />
-        </Box>
-      </Provider>
-    </ThemeProvider>
+              <Header title="White's Planner" />
+              <Box
+                sx={{
+                  height: "100%",
+                }}
+              >
+                <Component {...pageProps} />
+              </Box>
+              <Footer />
+            </Box>
+          </Provider>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </CssBaseline>
   );
 }
 
