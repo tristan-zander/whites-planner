f591e18942e4693d1c0676734e4e78ccc4cbe93f
Tristan Zander <tristannzander@gmail.com>
2021-12-19 18:45:40 -0500

    Finished database Update and Delete ops

--- features/normalize.js
+++ features/normalize.js
@@ -1,3 +1,5 @@
+import { Collection, Ref } from "faunadb";
+
 export function normalizeDbObject(obj) {
   // TODO: Verify if this is always the way the Fauna sends refs back as JSON
   let normalized = {
@@ -19,4 +21,31 @@ export function normalizeRef(ref) {
     nestedCollection: ref.value.collection.value?.collection.value.id,
   };
   return normalized;
+}
+
+export function undoNormalizeDbObject(normalized) {
+  let obj = { data: {}, ts: null, ref: {} };
+  Object.entries(normalized).forEach(([key, value]) => {
+    if (key == "ts") {
+      obj.ts = value;
+      return;
+    }
+    if (key == "ref") {
+      // The ref will stay normalized for now.
+      obj.ref = value;
+      return;
+    }
+    if (key == "owner") {
+      obj.data.owner = undoNormalizeRef(value);
+      return;
+    }
+    obj.data[key] = value;
+  });
+  return obj;
+}
+
+export function undoNormalizeRef(ref) {
+  let obj = Ref(Collection(ref.collection), ref.id);
+  console.debug(obj, obj.toJSON());
+  return obj.toJSON();
 }
